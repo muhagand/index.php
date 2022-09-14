af5d68b560826f6a9f7250f92f94590c2d81bd53
qmak4398 <85492366+muhagand@users.noreply.github.com>
2022-09-14 18:38:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login <Form></title>
+    <Link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
+<!-- <h2>Login Form <h2> -->
+
+<form action="tampil.php" method="POST">
+  <h2>Form Login </h2>
+  <div class="Imgcontainer">
+      <img scr="icon.png" alt="Avatar" class="Avatar">
+  </div>
+
+  <div class="container">
+    <label><b>Username</b></label>
+      <input type="text" Placeholder="Masukan Username" name="email" required>
+      <button type="sumbit">Masuk</Button>
+  
+  <input type="checkbox" checked="checked"><span>Ingat Saya</spam></div>
+  
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Batal</button>
+    <spam class="pws">Lupa <a href="#">password?</a></spam>
+    </div>
+</from>
+
+</body>
+</html>
